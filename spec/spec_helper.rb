498b903c61d0ebaa86d04e926be13d586f73745d
Bruno Vicenzo <bruno@vicenzo.me>
2023-03-02 13:39:02 -0300

    Add spec support helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter 'spec'
+  add_filter 'vendor'
+
+  enable_coverage :branch
+end
+
+require 'bundler/setup'
+require 'webmock/rspec'
+require 'f_service/rspec'
+
 require 'ploomes_client'
+
+Dir[File.join(File.expand_path(__dir__), 'support/**/*.rb')].each { |file| require file }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
---
+++ spec/support/dry_configurable.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Mor info at: https://dry-rb.org/gems/dry-configurable/0.11/testing/
+
+require 'dry/configurable/test_interface'
+
+# PloomesClient::Configuration.enable_test_interface
+#
+# RSpec.configure do |config|
+#   config.before { PloomesClient::Configuration.reset_config }
+# end
